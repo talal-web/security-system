61eca6c2beade2890a2bbec6e67030ee5f00a1b6
talal-web <maliktalal910@gmail.com>
2026-05-14 09:21:21 +0500

    Add option reuseable data

---
+++ frontend/constants/employee.ts
@@ -0,0 +1,19 @@
+export const educationOptions = [
+  "nil",
+  "middle",
+  "matric",
+  "fsc",
+  "intermediate",
+  "bs",
+  "master",
+];
+
+export const designationOptions = [
+  "guard",
+  "army guard",
+  "asst supervisor",
+  "supervisor",
+  "mcr",
+  "driver",
+  "clerk",
+];
