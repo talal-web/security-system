d9da8ce62d9f792e48e32cd868e8c6b0620e34bc
talal-web <maliktalal910@gmail.com>
2026-05-21 10:31:20 +0500

    Add api services for auth, employee and location

---
+++ frontend/services/location.service.ts
@@ -0,0 +1,72 @@
+// services/location.service.ts
+
+import api from "@/lib/axios";
+
+import { getApiErrorMessage } from "@/lib/apiError";
+
+import {
+  ILocation,
+  CreateLocationPayload,
+  UpdateLocationPayload,
+} from "@/types/location";
+
+// ================= CREATE LOCATION =================
+export const createLocation = async (
+  payload: CreateLocationPayload,
+): Promise<ILocation> => {
+  try {
+    const response = await api.post("/locations", payload);
+
+    return response.data.data;
+  } catch (error) {
+    throw new Error(getApiErrorMessage(error));
+  }
+};
+
+// ================= GET ALL LOCATIONS =================
+export const getLocations = async (): Promise<ILocation[]> => {
+  try {
+    const response = await api.get("/locations");
+
+    return response.data.data;
+  } catch (error) {
+    throw new Error(getApiErrorMessage(error));
+  }
+};
+
+// ================= GET SINGLE LOCATION =================
+export const getLocationById = async (id: string): Promise<ILocation> => {
+  try {
+    const response = await api.get(`/locations/${id}`);
+
+    return response.data.data;
+  } catch (error) {
+    throw new Error(getApiErrorMessage(error));
+  }
+};
+
+// ================= UPDATE LOCATION =================
+export const updateLocation = async ({
+  id,
+  payload,
+}: {
+  id: string;
+  payload: UpdateLocationPayload;
+}): Promise<ILocation> => {
+  try {
+    const response = await api.put(`/locations/${id}`, payload);
+
+    return response.data.data;
+  } catch (error) {
+    throw new Error(getApiErrorMessage(error));
+  }
+};
+
+// ================= DELETE LOCATION =================
+export const deleteLocation = async (id: string): Promise<void> => {
+  try {
+    await api.delete(`/locations/${id}`);
+  } catch (error) {
+    throw new Error(getApiErrorMessage(error));
+  }
+};
